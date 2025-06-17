d45cb97330f57aa63d6987c0d947eef4674339d5
lacyadale <lacyadale@gmail.com>
2025-06-16 21:49:48 -0500

    Delete lib directory

--- lib/state.jsx
+++
@@ -1,6 +0,0 @@
-import { useState } from 'react';
-
-export function useEmotionState(defaultValue = '') {
-  const [emotion, setEmotion] = useState(defaultValue);
-  return { emotion, setEmotion };
-}
